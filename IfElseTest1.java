ac070f8fbfc68a9009637fbd59401c398d68ca74
zhang4apps <133307742+zhang4apps@users.noreply.github.com>
2023-11-15 14:42:07 +0800

    Update IfElseTest1.java

--- IfElseTest1.java
+++ IfElseTest1.java
@@ -32,20 +32,66 @@ class IfElseTest1 {
 		}
 		*/
 		
-		//方式2：
-		score = 88;
-
+		//方式1：
+		/*
 		if(score == 100){
 			System.out.println("奖励一辆跑车");
-		}else if(score > 80){
+		}else if(score > 80 && score <= 99){
 			System.out.println("奖励一辆山地自行车");
-		}else if(score >= 60){
+		}else if(score >= 60 && score <= 80){
 			System.out.println("奖励环球影城一日游");
 		}else{
 			System.out.println("胖揍一顿");
 		}
-int score = 61;
-
+		*/
+		//方式1：
+		/*
+		if(score == 100){
+			System.out.println("奖励一辆跑车");
+		}else if(score > 80 && score <= 99){
+			System.out.println("奖励一辆山地自行车");
+		}else if(score >= 60 && score <= 80){
+			System.out.println("奖励环球影城一日游");
+		}else{
+			System.out.println("胖揍一顿");
+		}
+		*/
+		//方式1：
+		/*
+		if(score == 100){
+			System.out.println("奖励一辆跑车");
+		}else if(score > 80 && score <= 99){
+			System.out.println("奖励一辆山地自行车");
+		}else if(score >= 60 && score <= 80){
+			System.out.println("奖励环球影城一日游");
+		}else{
+			System.out.println("胖揍一顿");
+		}
+		*/
+		//方式1：
+		/*
+		if(score == 100){
+			System.out.println("奖励一辆跑车");
+		}else if(score > 80 && score <= 99){
+			System.out.println("奖励一辆山地自行车");
+		}else if(score >= 60 && score <= 80){
+			System.out.println("奖励环球影城一日游");
+		}else{
+			System.out.println("胖揍一顿");
+		}
+		*/
+		//方式1：
+		/*
+		if(score == 100){
+			System.out.println("奖励一辆跑车");
+		}else if(score > 80 && score <= 99){
+			System.out.println("奖励一辆山地自行车");
+		}else if(score >= 60 && score <= 80){
+			System.out.println("奖励环球影城一日游");
+		}else{
+			System.out.println("胖揍一顿");
+		}
+		*/
 		//方式1：
 		/*
 		if(score == 100){
@@ -59,20 +105,6 @@ int score = 61;
 		}
 		*/
 		
-		//特别的：
-		if(score == 100){
-			System.out.println("奖励一辆跑车");
-		}else if(score > 80){
-			System.out.println("奖励一辆山地自行车");
-		}else if(score >= 60){
-			System.out.println("奖励环球影城一日游");
-		}
-		/*else{
-			System.out.println("胖揍一顿");
-		}
-		*/
-		int score = 61;
-
 		//方式1：
 		/*
 		if(score == 100){
